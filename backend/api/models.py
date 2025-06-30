737bec1b086fe62117ee0bff5ebc02ac52e0b7b2
stevenpyae <steven.pyaelinkyaw@gmail.com>
2025-07-01 01:41:30 +0800

    Implement Teacher Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    class_size = models.PositiveIntegerField() # Number of students the teacher can handle
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
